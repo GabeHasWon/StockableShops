2785df6cb8319c4e766b03a73c03429034323922
TigerChenzzz <1139719265@qq.com>
2024-09-26 11:57:26 +0800

    Set an item to buyOnce rather than isAShopItem, like other overloads

--- Stock/StockedShop.cs
+++ Stock/StockedShop.cs
@@ -293,7 +293,7 @@ public abstract class StockedShop : ModType
         {
             Condition = AlwaysTrue;
             Item = item;
-            Item.isAShopItem = true;
+            Item.buyOnce = true;
         }
 
         /// <summary>
